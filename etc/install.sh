fc371f1a02076f480f1887f4937b4eff251d5aee
Neuralis-AI <64447278+Neuralis-AI@users.noreply.github.com>
2020-11-30 10:12:27 +0100

    Update install.sh

--- etc/install.sh
+++ etc/install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
-
-
-
-
-
-
-
-
+apt install -y python3-pil 
+pip3 install adafruit-circuitpython-ssd1306
+pip3 install adafruit-circuitpython-mcp230xx
 
 wget "health.pyurl" -O "/home/s360box/logs/health.py"
 chmod a+x /home/s360box/logs/health.py
+
+print("All done! Now pull the plug daddy... 👁️👅👁️")
