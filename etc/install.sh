a6968b8c7ecf00ee345929bb55d86773d1d9039e
Neuralis-AI <64447278+Neuralis-AI@users.noreply.github.com>
2020-11-30 10:28:56 +0100

    Update install.sh

--- etc/install.sh
+++ etc/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 apt install -y python3-pil 
 pip3 install adafruit-circuitpython-ssd1306
 pip3 install adafruit-circuitpython-mcp230xx
@@ -9,5 +10,5 @@ touch /home/s360box/logs/internet-check
 echo 1 > /home/s360box/logs/internet-check
 chmod 777 /home/s360box/logs/internet-check
 chmod a+x /home/s360box/logs/health.py
-
+clear
 print("All done! Now pull the plug daddy... (♥_♥)")
