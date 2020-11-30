3b279d9e89b754ed952ff7b47da4e115d120cec4
Neuralis-AI <64447278+Neuralis-AI@users.noreply.github.com>
2020-11-30 10:18:25 +0100

    Update install.sh

--- etc/install.sh
+++ etc/install.sh
@@ -3,7 +3,11 @@ apt install -y python3-pil
 pip3 install adafruit-circuitpython-ssd1306
 pip3 install adafruit-circuitpython-mcp230xx
 
-wget "health.pyurl" -O "/home/s360box/logs/health.py"
+wget https://raw.githubusercontent.com/Neuralis-AI/LCD-show/master/etc/health.py -O "/home/s360box/logs/health.py"
+
+touch /home/s360box/logs/internet-check
+echo 1 > /home/s360box/logs/internet-check
+chmod 777 /home/s360box/logs/internet-check
 chmod a+x /home/s360box/logs/health.py
 
 print("All done! Now pull the plug daddy... 👁️👅👁️")
